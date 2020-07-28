65e8df0646e5a1b9f2ded8ba4f29067dfcd726dd
oleg <vinodevmail@gmail.com>
2020-07-28 16:25:40 +0300

    Updates

---
+++ precache-manifest.046b530365c26f88939f71227c3ea767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a7fa16637d5110a4bad8df7fbe7e6f",
+    "url": "/covid_19_dashboard/index.html"
+  },
+  {
+    "revision": "307d42c7c132d6f207bc",
+    "url": "/covid_19_dashboard/static/css/main.00f0b84b.chunk.css"
+  },
+  {
+    "revision": "e86fdf9bb465f912665f",
+    "url": "/covid_19_dashboard/static/js/2.d28290a1.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid_19_dashboard/static/js/2.d28290a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307d42c7c132d6f207bc",
+    "url": "/covid_19_dashboard/static/js/main.0a780f05.chunk.js"
+  },
+  {
+    "revision": "601f74c1d3bcf4b9e022",
+    "url": "/covid_19_dashboard/static/js/runtime-main.f9887423.js"
+  }
+]);
